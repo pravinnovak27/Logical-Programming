9cf8a2396445b4e56819fe0fb4c6d36f414efdab
Pravin Novak <pravinnovak2707@gmail.com>
2022-11-25 00:33:53 +0530

    added perfect number program

---
+++ src/main/java/com/bridgelabz/PerfectNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class PerfectNumber {
+    public static void main(String[] args) {
+		
+		int n,sum=0;
+		System.out.println("Enter the number : ");
+		Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();	
+        
+        for(int i=1; i<n; i++) {
+        	if(n%i==0) {
+        		sum = sum+i;
+        	}
+        }
+        if(n==sum)
+        	System.out.println("Perfect Number");
+        else
+        	System.out.println("Not A Perfect Number");
+            sc.close();
+	}
+}
