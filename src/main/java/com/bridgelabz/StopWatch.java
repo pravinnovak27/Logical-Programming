33e675757c8b079954c2842931879ef932457783
Pravin Novak <pravinnovak2707@gmail.com>
2022-11-25 00:40:30 +0530

    added stopwatch program

---
+++ src/main/java/com/bridgelabz/StopWatch.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+import java.math.BigInteger;
+import java.time.Duration;
+import java.time.Instant;
+
+
+public class StopWatch {
+    
+    public static void main(String[] args) {
+
+        Instant instantStarted = Instant.now();
+        Fibonacci(40);
+        Instant instantStopped = Instant.now();
+
+        Duration durationBetween = Duration.between(instantStarted, instantStopped);
+
+        System.out.println("The elapsed time in milliseconds: " + durationBetween.toMillis());
+        System.out.println("The elapsed time in seconds: " + durationBetween.toSeconds());
+        System.out.println("The elapsed time in minutes: " + durationBetween.toMinutes());
+
+
+    }
+
+    private static BigInteger Fibonacci(int n) {
+        if (n < 2)
+            return BigInteger.ONE;
+        else
+            return Fibonacci(n - 1).add(Fibonacci(n - 2));
+    }
+}
+
